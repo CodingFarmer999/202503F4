8be33dab136a136f1ef0eda3901196d08d3bd428
CodingFarmer999 <codingfarmer999@gmail.com>
2025-03-01 14:22:24 +0800

    DI

--- 00_maven/src/main/java/com/bean/Main.java
+++ 00_maven/src/main/java/com/bean/Main.java
@@ -3,8 +3,15 @@ package com.bean;
 public class Main {
 
 	public static void main(String[] args) {
-		User user= new User();
-		user.setName("");
+//		User user= new User();
+//		user.setName("");
+		
+		Toyota toyota = new Toyota(new ToyotaEngine());
+		
+		
+		Toyota toyota2 = new Toyota();
+		toyota2.setEngine(new BenzEngine());
+		toyota2.move();
 	}
 
 }
