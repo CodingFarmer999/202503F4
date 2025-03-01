ea459255fb366e15f872648a26a69be4cb730a9f
CodingFarmer999 <codingfarmer999@gmail.com>
2025-03-01 14:58:00 +0800

    Bean

--- 01_hellospring/src/main/java/SpringMain.java
+++ 01_hellospring/src/main/java/SpringMain.java
@@ -1,12 +1,21 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import model.Toyota;
+
 public class SpringMain {
 
 	public static void main(String[] args) {
 		// Spring IoC 容器
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("bean.xml");
 
+		String[] beans = ctx.getBeanDefinitionNames();
+		for (String name : beans) {
+			System.out.println(name);
+		}
+		
+		Toyota car = ctx.getBean(Toyota.class);
+		car.move();
 	}
 
 }
