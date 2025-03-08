a2e03ab57d2f062cd5d3fca99c8364821dd7742d
CodingFarmer999 <codingfarmer999@gmail.com>
2025-03-08 14:27:33 +0800

    request param

--- 02_springmvc/src/main/java/controller/LoginController.java
+++ 02_springmvc/src/main/java/controller/LoginController.java
@@ -2,13 +2,14 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //@Component
 @Controller
 public class LoginController {
 
 	@RequestMapping("/login")
-	public String login(String username, String password) {
+	public String login(@RequestParam("username2") String username, @RequestParam("password2") String password) {
 		
 		System.out.println("username: " + username);
 		System.out.println("password: " + password);
