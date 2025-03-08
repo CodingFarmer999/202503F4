5d10aed9fd118ddd38289008a77fd386188e251d
CodingFarmer999 <codingfarmer999@gmail.com>
2025-03-08 10:51:08 +0800

    ConfigurableBeanFactory

--- 01_hellospring/src/main/java/model/ProtoTypeBean.java
+++ 01_hellospring/src/main/java/model/ProtoTypeBean.java
@@ -1,10 +1,12 @@
 package model;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+//@Scope("prototype")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class ProtoTypeBean {
 
 }
