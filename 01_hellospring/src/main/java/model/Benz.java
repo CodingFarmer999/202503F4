9b713d0447bcba5e7580fcdf1027ee9248f4e692
CodingFarmer999 <codingfarmer999@gmail.com>
2025-03-08 10:20:01 +0800

    Quarifier

--- 01_hellospring/src/main/java/model/Benz.java
+++ 01_hellospring/src/main/java/model/Benz.java
@@ -1,12 +1,14 @@
 package model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Benz {
 
 	@Autowired
+	@Qualifier("kiaEngine")
 	private Engine engine;
 	
 	public Benz() {
