b89354e0b7d5a4ff4475bfd9716bbbd7410a3a11
CodingFarmer999 <codingfarmer999@gmail.com>
2025-03-15 12:02:20 +0800

    session

--- 03_exp_guess/src/main/java/com/course/model/GameInfo.java
+++ 03_exp_guess/src/main/java/com/course/model/GameInfo.java
@@ -3,6 +3,12 @@ package com.course.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class GameInfo {
 
 	/** 本次答案 */
--- 03_exp_guess/src/main/java/com/course/service/GuessService.java
+++ 03_exp_guess/src/main/java/com/course/service/GuessService.java
@@ -5,11 +5,16 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.course.model.GameInfo;
 
 @Service
 public class GuessService {
 
+	@Autowired
+	private GameInfo gameInfo;
 	
 	public String getAnswer() {
 		// {"8, "3", "5", "1"}
@@ -28,6 +33,12 @@ public class GuessService {
 		String answerStr = answerList.stream().map(n -> String.valueOf(n)).collect(Collectors.joining());
 		
 		System.out.println("answer: " + answerStr);
+		gameInfo.setAnswer(answerStr);
 		return answerStr;
+	}
+	
+	public void testSession() {
+		String answer = gameInfo.getAnswer();
+		System.out.println(answer);
 	}
 }
