f833c1fe45f7c8b418a348aabd7e5027e81c408b
CodingFarmer999 <codingfarmer999@gmail.com>
2025-03-15 16:42:08 +0800

    Add User

--- 04_springmvc_hibernate/src/main/java/com/course/dao/UserDaoImpl.java
+++ 04_springmvc_hibernate/src/main/java/com/course/dao/UserDaoImpl.java
@@ -1,5 +1,9 @@
 package com.course.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Repository;
 
 import com.course.entity.User;
@@ -15,7 +19,21 @@ public class UserDaoImpl implements UserDao {
 
 	@Override
 	public void addUser(String username, String password) {
+		User user = new User(username, password);
 		System.out.println("Add User");
+		
+		Configuration configuration = new Configuration().configure();
+	    SessionFactory sessionFactory = configuration.buildSessionFactory();
+	    // 取得Session
+	    Session session = sessionFactory.openSession();
+		// 開啟交易/事務
+		Transaction transaction = session.beginTransaction();
+		// 新增
+		session.save(user);
+		// 提交
+		transaction.commit();
+		// 關閉連線
+		session.close();
 		
 	}
 
