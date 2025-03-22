4e690c60deb67c81cf2f9de83b24a6bd0c8514b8
CodingFarmer999 <codingfarmer999@gmail.com>
2025-03-22 15:25:04 +0800

    getter

--- 04_springmvc_hibernate/src/main/java/com/course/entity/User.java
+++ 04_springmvc_hibernate/src/main/java/com/course/entity/User.java
@@ -62,5 +62,21 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDay() {
+		return birthDay;
+	}
+
+	public void setBirthDay(Date birthDay) {
+		this.birthDay = birthDay;
+	}
 	
 }
