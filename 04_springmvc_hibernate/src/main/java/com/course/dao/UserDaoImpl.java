cb244424c3120dc53f4907b575f728633d3a593b
CodingFarmer999 <codingfarmer999@gmail.com>
2025-03-22 10:06:28 +0800

    Try

--- 04_springmvc_hibernate/src/main/java/com/course/dao/UserDaoImpl.java
+++ 04_springmvc_hibernate/src/main/java/com/course/dao/UserDaoImpl.java
@@ -4,13 +4,18 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.course.entity.User;
+import com.course.service.ConnectionService;
 
 @Repository
 public class UserDaoImpl implements UserDao {
 
+	@Autowired
+	private ConnectionService connectionService;
+	
 	@Override
 	public User findByUsername(String username) {
 		// 呼叫DB
@@ -23,18 +28,26 @@ public class UserDaoImpl implements UserDao {
 		User user = new User(username, password);
 		System.out.println("Add User");
 		
-		Configuration configuration = new Configuration().configure();
-	    SessionFactory sessionFactory = configuration.buildSessionFactory();
-	    // 取得Session
-	    Session session = sessionFactory.openSession();
-		// 開啟交易/事務
-		Transaction transaction = session.beginTransaction();
-		// 新增
-		session.save(user);
-		// 提交
-		transaction.commit();
-		// 關閉連線
-		session.close();
+//		Configuration configuration = new Configuration().configure();
+//	    SessionFactory sessionFactory = configuration.buildSessionFactory();
+//	    // 取得Session
+//	    Session session = sessionFactory.openSession();
+	    
+	    
+	    //  Try With Resource
+	    try (Session session = connectionService.getSession();) {
+			// 開啟交易/事務
+			Transaction transaction = session.beginTransaction();
+			// 新增
+			session.save(user);
+			// 提交
+			transaction.commit();
+			
+			// 關閉連線
+			// session.close();
+	    }
+
+
 		
 	}
 
