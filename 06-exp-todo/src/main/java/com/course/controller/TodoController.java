d0520c3fc5df984c853e7237ca939b04ad7c6fec
CodingFarmer999 <codingfarmer999@gmail.com>
2025-03-29 16:04:51 +0800

    todo exp

--- 06-exp-todo/src/main/java/com/course/controller/TodoController.java
+++ 06-exp-todo/src/main/java/com/course/controller/TodoController.java
@@ -1,5 +1,8 @@
 package com.course.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class TodoController {
 
 }
--- 06-exp-todo/src/main/java/com/course/service/TodoService.java
+++ 06-exp-todo/src/main/java/com/course/service/TodoService.java
@@ -1,5 +1,13 @@
 package com.course.service;
 
-public class TodoService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.course.dao.TodoDao;
+
+@Service
+public class TodoService {
+	
+	@Autowired
+	private TodoDao todoDao;
 }
