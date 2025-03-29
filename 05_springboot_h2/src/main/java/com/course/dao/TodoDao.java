a21e49a4064c5d2be416f63e456b3e51502fde6e
CodingFarmer999 <codingfarmer999@gmail.com>
2025-03-29 13:52:24 +0800

    Dao

--- 05_springboot_h2/src/main/java/com/course/dao/TodoDao.java
+++ 05_springboot_h2/src/main/java/com/course/dao/TodoDao.java
@@ -1,5 +1,12 @@
 package com.course.dao;
 
+import com.course.model.TodoDto;
+
 public interface TodoDao {
 
+	void add(TodoDto todoDto);
+	
+	void update(TodoDto todoDto);
+	
+	void delete(Long id);
 }
--- 05_springboot_h2/src/main/java/com/course/dao/impl/TodoDaoImpl.java
+++ 05_springboot_h2/src/main/java/com/course/dao/impl/TodoDaoImpl.java
@@ -1,10 +1,34 @@
 package com.course.dao.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.course.dao.TodoDao;
+import com.course.model.TodoDto;
 
 @Repository
 public class TodoDaoImpl implements TodoDao {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Override
+	public void add(TodoDto todoDto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(TodoDto todoDto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
