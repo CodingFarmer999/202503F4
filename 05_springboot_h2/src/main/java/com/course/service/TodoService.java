197900413007bcf2c56e3e63feeb1204b8290ad1
CodingFarmer999 <codingfarmer999@gmail.com>
2025-03-29 14:21:39 +0800

    TEST

--- 05_springboot_h2/src/main/java/com/course/service/TodoService.java
+++ 05_springboot_h2/src/main/java/com/course/service/TodoService.java
@@ -16,7 +16,7 @@ public class TodoService {
 	
 	public void addTodo() {
 		TodoDto dto = new TodoDto();
-		dto.setTitle("吃飯");
+		dto.setTitle("吃飯2");
 		dto.setDueDate(new Date());
 		dto.setStatus(0);
 		todoDao.add(dto);
