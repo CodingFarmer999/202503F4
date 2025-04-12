a6d9b1d511dfb561d17963403560b71f131cc8d4
CodingFarmer999 <codingfarmer999@gmail.com>
2025-04-12 11:11:31 +0800

    todo

--- 06-exp-todo/src/main/java/com/course/controller/TodoController.java
+++ 06-exp-todo/src/main/java/com/course/controller/TodoController.java
@@ -1,14 +1,24 @@
 package com.course.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.course.model.TodoVo;
+import com.course.service.TodoService;
+
 @Controller
 public class TodoController {
 
+	@Autowired
+	private TodoService todoService;
+	
 	@GetMapping("/")
 	public String home() {
+		List<TodoVo> todoList= todoService.getAllTodoList();
 		return "index";
 	}
 	
--- 06-exp-todo/src/main/java/com/course/service/TodoService.java
+++ 06-exp-todo/src/main/java/com/course/service/TodoService.java
@@ -1,13 +1,60 @@
 package com.course.service;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.course.dao.TodoDao;
+import com.course.model.TodoDto;
+import com.course.model.TodoVo;
 
 @Service
 public class TodoService {
 	
 	@Autowired
 	private TodoDao todoDao;
+	
+	@Autowired
+	private TodoServiceHelper helper;
+	
+	public List<TodoVo> getAllTodoList() {
+		List<TodoDto> dtoList = todoDao.findAll();
+		
+//		for (TodoDto dto : dtoList) {
+//			dto.setMemo("XXXX");
+//		}
+		
+//		dtoList.stream().forEach(d -> d.setMemo("XXXX"));
+		
+		List<TodoVo> result = dtoList.stream().map(dto -> helper.convertToVo(dto)).collect(Collectors.toList());
+		
+		List<TodoVo> resultList = new ArrayList<>();
+		for (TodoDto todoDto : dtoList) {
+//			TodoVo vo = new TodoVo();
+//			vo.setId(todoDto.getId());
+//			vo.setTitle(todoDto.getTitle());
+//			vo.setDueDate(parseDateToString(todoDto.getDueDate()));
+//			vo.setStatus(String.valueOf(todoDto.getStatus()));
+//			vo.setStatusDisp(todoDto.getStatus() == 0 ? "未完成" : "已完成");
+//			vo.setMemo(todoDto.getMemo());
+//			vo.setUsername(todoDto.getUsername());
+			TodoVo vo = helper.convertToVo(todoDto);
+			resultList.add(vo);
+
+		}
+		
+		return dtoList.stream().map(dto -> helper.convertToVo(dto)).collect(Collectors.toList());
+	}
+	
+	private String parseDateToString(Date date) {
+	     // 定義日期格式
+       SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+       // 將 Date 物件轉換為 String
+       return formatter.format(date);
+	}
 }
