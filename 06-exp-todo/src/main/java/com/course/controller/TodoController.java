326c9be01f64d9ca0ac887e964591c8ee0f5911f
CodingFarmer999 <codingfarmer999@gmail.com>
2025-04-12 11:35:06 +0800

    th each

--- 06-exp-todo/src/main/java/com/course/controller/TodoController.java
+++ 06-exp-todo/src/main/java/com/course/controller/TodoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -17,8 +18,9 @@ public class TodoController {
 	private TodoService todoService;
 	
 	@GetMapping("/")
-	public String home() {
+	public String home(Model model) {
 		List<TodoVo> todoList= todoService.getAllTodoList();
+		model.addAttribute("todoList", todoList);
 		return "index";
 	}
 	
