6484896846e273a2bd8c01122e6abcf9b8d42bb4
CodingFarmer999 <codingfarmer999@gmail.com>
2025-04-12 16:04:12 +0800

    request body

--- 07_springboot_restful/src/main/java/com/course/controller/RestfulController.java
+++ 07_springboot_restful/src/main/java/com/course/controller/RestfulController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +39,14 @@ public class RestfulController {
 	@Operation(summary = "新增使用者", description = "新增使用者", tags = { "USER-API" })
 	@PostMapping("/user")
 	public User addUser(User user) {
+		System.out.println("user: " + user);
+		// TODO: 新增使用者，自己做
+		return user;
+	}
+	
+	@Operation(summary = "新增使用者 RequestBody", description = "新增使用者", tags = { "USER-API" })
+	@PostMapping("/user2")
+	public User addUser2(@RequestBody User user) {
 		System.out.println("user: " + user);
 		// TODO: 新增使用者，自己做
 		return user;
