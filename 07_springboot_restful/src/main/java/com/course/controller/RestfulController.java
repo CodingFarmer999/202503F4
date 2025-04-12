e6bd1c91642a0ae83a5e6dc70f52f49dc166fc57
CodingFarmer999 <codingfarmer999@gmail.com>
2025-04-12 15:12:01 +0800

    user

--- 07_springboot_restful/src/main/java/com/course/controller/RestfulController.java
+++ 07_springboot_restful/src/main/java/com/course/controller/RestfulController.java
@@ -1,7 +1,12 @@
 package com.course.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.course.model.User;
 
 //@Controller
 //@ResponseBody
@@ -11,5 +16,13 @@ public class RestfulController {
 	@GetMapping("/home")
 	public String home() {
 		return "home";
+	}
+	
+	@GetMapping("/users")
+	public List<User> getAllUser() {
+		User user1 = new User("Kitty", "aaa");
+		User user2 = new User("Snoopy", "bbb");
+		User user3 = new User("Bird", "ccc");
+		return Arrays.asList(user1, user2, user3);
 	}
 }
