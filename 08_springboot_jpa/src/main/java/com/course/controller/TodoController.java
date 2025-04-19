1154ebdf232f2fab3583ff9d6f0445b60d369f3f
CodingFarmer999 <codingfarmer999@gmail.com>
2025-04-19 12:16:15 +0800

    findAll

--- 08_springboot_jpa/src/main/java/com/course/controller/TodoController.java
+++ 08_springboot_jpa/src/main/java/com/course/controller/TodoController.java
@@ -1,8 +1,12 @@
 package com.course.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.course.entity.TodoEntity;
 import com.course.service.TodoService;
 
 @RestController
@@ -10,4 +14,9 @@ public class TodoController {
 
 	@Autowired
 	private TodoService todoService;
+	
+	@GetMapping("/todos")
+	public List<TodoEntity> getAllTodoList() {
+		return todoService.getAllTodo();
+	}
 }
