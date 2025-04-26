ac9cb45dd344c408c89e14521dd4a5e0e8c6bbcd
CodingFarmer999 <codingfarmer999@gmail.com>
2025-04-26 11:35:16 +0800

    Repository

--- 09-exp-ec/src/main/java/com/course/repository/ProductCustomRepository.java
+++ 09-exp-ec/src/main/java/com/course/repository/ProductCustomRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import com.course.dto.ProductDto;
 import com.course.vo.ProductQueryParam;
 
+@Repository
 public class ProductCustomRepository {
 	
 	// EntityManager
