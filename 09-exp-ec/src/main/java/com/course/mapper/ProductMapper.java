07c82fee31a2a14508898bdd5d0923d4fa727b5a
CodingFarmer999 <codingfarmer999@gmail.com>
2025-05-03 10:08:17 +0800

    findAll

--- 09-exp-ec/src/main/java/com/course/mapper/ProductMapper.java
+++ 09-exp-ec/src/main/java/com/course/mapper/ProductMapper.java
@@ -1,8 +1,15 @@
 package com.course.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.course.dto.ProductDto;
 
 @Mapper
 public interface ProductMapper {
 
+	@Select("SELECT * FROM PRODUCT")
+	List<ProductDto> findAll();
 }
