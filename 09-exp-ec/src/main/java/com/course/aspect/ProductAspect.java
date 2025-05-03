40e8427f98c537ff41952f617eb8041203887f23
CodingFarmer999 <codingfarmer999@gmail.com>
2025-05-03 16:03:49 +0800

    AfterReturn

--- 09-exp-ec/src/main/java/com/course/aspect/ProductAspect.java
+++ 09-exp-ec/src/main/java/com/course/aspect/ProductAspect.java
@@ -2,13 +2,16 @@ package com.course.aspect;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import com.course.vo.ProductVo;
 
 @Component
 @Aspect
@@ -27,7 +30,7 @@ public class ProductAspect {
 	    logger.info("切面 AopAspect.beforeAdvice() -> @Before通知，在方法調用前先行調用，被切的方法名稱：" + joinPoint.getSignature().getName());
 	}
 	
-	@Around("pointCutMethod()")
+//	@Around("pointCutMethod()")
 	public void aroundAdvice(ProceedingJoinPoint joinPoint) throws Throwable {
 	    logger.info("切面 AopAspect.aroundAdvice() -> @Around通知 proceed之前，被切的方法名稱：" + joinPoint.getSignature().getName());
 	    // 需要Throw Exception
@@ -35,5 +38,17 @@ public class ProductAspect {
 	    joinPoint.proceed();
  
 	    logger.info("切面 AopAspect.aroundAdvice() -> @Around通知 proceed之後");
+	}
+	
+	@AfterReturning(value= "pointCutMethod()", returning = "result")
+	public void afterReturnAdvice(JoinPoint joinPoint, ProductVo result) {
+	    logger.info("切面 AopAspect.afterReturnAdvice() -> @AfterReturning通知，在方法調用之後才調用，被切的方法名稱：" + joinPoint.getSignature().getName());
+	    logger.info("切面 AopAspect.afterReturnAdvice() -> @AfterReturning通知，在方法調用之後才調用，被切的方法的回傳值：" + result);
+	}
+	
+	@AfterThrowing(value= "pointCutMethod()", throwing = "ex")
+	public void afterThrowiungAdvice(JoinPoint joinPoint, Throwable ex) {
+	    logger.info("切面 AopAspect.afterThrowiungAdvice() -> @AfterThrowing通知，在方法發生異常時才調用，被切的方法名稱：" + joinPoint.getSignature().getName());
+	    logger.info("切面 AopAspect.afterThrowiungAdvice() -> @AfterThrowing通知，異常例外：" + ex);
 	}
 }
