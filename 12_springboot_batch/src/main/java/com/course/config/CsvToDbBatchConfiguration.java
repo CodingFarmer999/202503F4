9b97709cb63f15a9e424cccd39da1e495617efa9
CodingFarmer999 <codingfarmer999@gmail.com>
2025-05-10 10:17:28 +0800

    config

---
+++ 12_springboot_batch/src/main/java/com/course/config/CsvToDbBatchConfiguration.java
@@ -0,0 +1,17 @@
+package com.course.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CsvToDbBatchConfiguration {
+
+	// Reader
+	
+	// Processor
+	
+	// Writer
+	
+	// Step
+	
+	// Job
+}
