2f9cdd7b4c4d90647348f7fe22da1595a493d2a9
CodingFarmer888 <>
2025-07-22 21:55:13 +0800

    Singleton

--- 01_hellospring/src/main/java/utils/SingletonUtil.java
+++ 01_hellospring/src/main/java/utils/SingletonUtil.java
@@ -1,15 +1,19 @@
 package utils;
 
+/**
+ * 單例 Singleton 設計模式
+ */
 public class SingletonUtil {
-//	私有建構式
-//	私有靜態的欄位，用來存放單例實體
-//	公有靜態的方法，來獲得單例實體
+
+	// 2. 私有靜態的欄位，用來存放單例實體
 	private static SingletonUtil instance;
 	
+	// 1. 私有的建構式，不能使用 new 的方式建立物件
 	private SingletonUtil() {
 		
 	}
 	
+	// 3. 公有靜態的方法，來獲得單例實體
 	public static SingletonUtil getInstance() {
         if (instance == null) {
         	// 首次呼叫才產生物件
